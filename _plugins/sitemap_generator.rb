dc3ca8d3624660cd3496a9b5cbd68594d0d82c8e
srinivas <sgontu@traveltripper.com>
2017-03-24 12:28:31 +0530

    sitemap

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -1,5 +1,6 @@
 require 'xkeys'
 
+require 'fileutils'
 Jekyll::Hooks.register :site, :post_write do |site|
   SitemapGenerator.new(site).generate
 end
@@ -33,8 +34,9 @@ class SitemapGenerator
       path = path[0..-2] + ['__PAGES__']
 
       source_path = page.is_a?(Jekyll::DataPage) ? page.source_path : page.path
+
        if page.data['published'] =='false'
-         exclude_data << source_path
+         exclude_data << page.path
       end
       sitemap[*path] ||= []
       sitemap[*path] << { label: page.data['label'] || page.data['title'] || label, published: page.data['published'], locales: localized_urls(site, page), data_source: (page.is_a?(Jekyll::DataPage) && page.data_source) || nil, source_path: source_path } unless page.data['editable'] === false
@@ -53,11 +55,13 @@ class SitemapGenerator
      if exclude_data.any?
        if  exclude_data != config_data["exclude"]
        config_data["exclude"] = exclude_data
-       save_config config_data["exclude"]
+       # save_config config_data["exclude"]
+       saving_file config_data["exclude"]
        end
      else
        config_data["exclude"] = []
-       save_config config_data["exclude"]
+       # save_config config_data["exclude"]
+       saving_file config_data["exclude"]
     end
     save sitemap
   end
@@ -77,36 +81,78 @@ class SitemapGenerator
   end
 
   def save_config(data)
-    # lines = File.readlines('_config.yml')
-a =    File.read('_config.yml').include?("exclude:")
-if a == false
-  File.open('_config.yml', 'r+') do |f|
-    # go back 2 from the end, to overwrite 1 character and the final \n
-    f.seek(0, IO::SEEK_END)
-    f.write("exclude: #{data}\n")
-  end
-end
-    File.open('_config.yml', 'r+') do |file|
-      file.each_line do |line|
-        # puts i+=1
-
-        if (line=~/exclude:/)
-          file.seek(-line.length+2, IO::SEEK_CUR)
-
-          # lines[i-1] = "exclude: #{data}"
-          # file.seek(-line.length-0, IO::SEEK_CUR)
-          # file.seek(-line.length, IO::SEEK_CUR)
-          file.write "exclude: #{data}"
-          # file.write(lines.join)
-          return
-        end
-
-
-      end
-    end
+    saving_file (data)
+#     binding.pry
+#     a = get_line_number '_config.yml','exclude:'
+#     binding.pry
+#     # lines = File.readlines('_config.yml')
+# a =    File.read('_config.yml').include?("exclude:")
+# if a == false
+#   File.open('_config.yml', 'r+') do |f|
+#     # go back 2 from the end, to overwrite 1 character and the final \n
+#     f.seek(0, IO::SEEK_END)
+#     f.write("exclude: #{data}\n")
+#   end
+# end
+#     File.open('_config.yml', 'r+') do |file|
+#       file.each_line do |line|
+#         # puts i+=1
+#
+#         if (line=~/exclude:/)
+#           file.seek(-line.length+2, IO::SEEK_CUR)
+#
+#           # lines[i-1] = "exclude: #{data}"
+#           # file.seek(-line.length-0, IO::SEEK_CUR)
+#           # file.seek(-line.length, IO::SEEK_CUR)
+#           file.write "exclude: #{data}"
+#           # file.write(lines.join)
+#           return
+#         end
+#
+#
+#       end
+#     end
   end
 
   def sha
     `git rev-parse HEAD`.chomp
+  end
+
+  def get_line_number(file, word)
+  count = 0
+  file = File.open(file, "r") { |file| file.each_line { |line|
+    count += 1
+    return count if line =~ /#{word}/
+  }}
+  file.close
+  end
+
+  def saving_file(data)
+    #---------
+    # first time data
+    #---------
+    a = get_line_number '_config.yml','exclude:'
+    if a.nil?
+      open('_config.yml','a') do |f|
+        f << "exclude: #{data}"
+      end
+      return
+    end
+#-----------
+# Data update
+#-----------
+    File.open("output_file", "w") do |out_file|
+      File.foreach('_config.yml') do |line|
+         if line =~ /exclude:/
+
+           out_file.puts "exclude: #{data}"
+           flag ='false'
+         else
+           out_file.puts line
+         end
+      end
+    end
+
+    FileUtils.mv("output_file", '_config.yml')
   end
 end
