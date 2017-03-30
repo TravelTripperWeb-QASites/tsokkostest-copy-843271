fd1e577db866c0fde525ddbe6531eeafbccc7283
srinivas <sgontu@traveltripper.com>
2017-03-30 11:17:00 +0530

    new files

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -1,5 +1,4 @@
 require 'xkeys'
-
 require 'fileutils'
 Jekyll::Hooks.register :site, :post_write do |site|
   SitemapGenerator.new(site).generate
@@ -18,26 +17,19 @@ class SitemapGenerator
     # generate only once
     return unless default_lang == site.active_lang
     sitemap = {}.extend(XKeys::Hash)
-
     sitemap['__CONFIG__', 'default_locale'] = default_lang
     sitemap['__CONFIG__', 'page_gen'] = site.config['page_gen']
     sitemap['__CONFIG__', 'locales'] = site.config['languages']
-    exclude_data = []
+
     pages.each do |page|
       url = page.url
       url += 'index.html' if url.end_with?('/')
-
       url = '__ROOT__' + url
-
       path = url.split('/')
       label = path.last == 'index.html' && path.length > 2 ? path[-2] : path.last
       path = path[0..-2] + ['__PAGES__']
-
       source_path = page.is_a?(Jekyll::DataPage) ? page.source_path : page.path
 
-       if page.data['published'] =='false'
-         exclude_data << page.path
-      end
       sitemap[*path] ||= []
       sitemap[*path] << { label: page.data['label'] || page.data['title'] || label, published: page.data['published'], locales: localized_urls(site, page), data_source: (page.is_a?(Jekyll::DataPage) && page.data_source) || nil, source_path: source_path } unless page.data['editable'] === false
     end
@@ -51,18 +43,7 @@ class SitemapGenerator
     else
       sitemap['__SHA__'] = sha
     end
-    config_data =  YAML.load(File.read("_config.yml"))
-     if exclude_data.any?
-       if  exclude_data != config_data["exclude"]
-       config_data["exclude"] = exclude_data
-       # save_config config_data["exclude"]
-       saving_file config_data["exclude"]
-       end
-     else
-       config_data["exclude"] = []
-       # save_config config_data["exclude"]
-       saving_file config_data["exclude"]
-    end
+    save_config(unpublished_files) unless unpublished_files.empty? # unpublished files will be saved in _config.yml
     save sitemap
   end
 
@@ -80,79 +61,44 @@ class SitemapGenerator
     end
   end
 
-  def save_config(data)
-    saving_file (data)
-#     binding.pry
-#     a = get_line_number '_config.yml','exclude:'
-#     binding.pry
-#     # lines = File.readlines('_config.yml')
-# a =    File.read('_config.yml').include?("exclude:")
-# if a == false
-#   File.open('_config.yml', 'r+') do |f|
-#     # go back 2 from the end, to overwrite 1 character and the final \n
-#     f.seek(0, IO::SEEK_END)
-#     f.write("exclude: #{data}\n")
-#   end
-# end
-#     File.open('_config.yml', 'r+') do |file|
-#       file.each_line do |line|
-#         # puts i+=1
-#
-#         if (line=~/exclude:/)
-#           file.seek(-line.length+2, IO::SEEK_CUR)
-#
-#           # lines[i-1] = "exclude: #{data}"
-#           # file.seek(-line.length-0, IO::SEEK_CUR)
-#           # file.seek(-line.length, IO::SEEK_CUR)
-#           file.write "exclude: #{data}"
-#           # file.write(lines.join)
-#           return
-#         end
-#
-#
-#       end
-#     end
-  end
-
   def sha
     `git rev-parse HEAD`.chomp
   end
 
-  def get_line_number(file, word)
-  count = 0
-  file = File.open(file, "r") { |file| file.each_line { |line|
-    count += 1
-    return count if line =~ /#{word}/
-  }}
-  file.close
+  def unpublished_files
+    h_file = []
+    # enumerate the .html and .md files
+    Dir.glob("#{Dir.pwd}/**/*.{html,md}").each do |cfile|
+      if File.file?(cfile)
+        file_text = File.read(cfile)
+        # check the string exists
+        if file_text.include?("published_localized:\n  en: 'false'") || file_text.include?("published_localized:\n  en: false")
+          # Add all the unpublished files to array
+          h_file << cfile.gsub("#{Dir.pwd}/", '')
+        end
+      end
+    end
+    h_file
   end
 
-  def saving_file(data)
-    #---------
+  def save_config(data)
     # first time data
-    #---------
-    a = get_line_number '_config.yml','exclude:'
-    if a.nil?
-      open('_config.yml','a') do |f|
+    unless File.readlines('_config.yml').grep(/exclude:/).any?
+      open('_config.yml', 'a') do |f|
         f << "exclude: #{data}"
       end
       return
     end
-#-----------
-# Data update
-#-----------
-    File.open("output_file", "w") do |out_file|
+    # Data update
+    File.open('output_file', 'w') do |out_file|
       File.foreach('_config.yml') do |line|
-         if line =~ /exclude:/
-
-           out_file.puts "exclude: #{data}"
-           flag ='false'
-         else
-           out_file.puts line
-         end
+        if line =~ /exclude:/
+          out_file.puts "exclude: #{data}"
+        else
+          out_file.puts line
+        end
       end
     end
-
-    FileUtils.mv("output_file", '_config.yml')
+    FileUtils.mv('output_file', '_config.yml')
   end
 end
